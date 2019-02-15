f826f3721a07d3b4345a345903f338601580f04f
Andrew Gibbs <asg1968@hotmail.co.uk>
2019-02-15 16:03:29 +0000

    test: added unit test (zeroBalanceTest)

---
+++ src/test/java/AccountTest.java
@@ -0,0 +1,20 @@
+import main.java.Account;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class AccountTest {
+
+  private Account account;
+
+  @Before
+  public void setup() {
+    account = new Account();
+  }
+
+  @Test
+  public void zeroBalanceTest() {
+    assertTrue(account.currentBalance() == 0.00);
+  }
+}
