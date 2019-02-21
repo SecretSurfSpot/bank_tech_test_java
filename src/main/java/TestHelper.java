1b7ccab855d060541011336838d3c18c23a74524
Andrew Gibbs <asg1968@hotmail.co.uk>
2019-02-21 18:14:57 +0000

    test: added printStatement feature test

--- src/main/java/TestHelper.java
+++ src/main/java/TestHelper.java
@@ -11,11 +11,20 @@ public class TestHelper {
   private SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
   public String strDate = simpleDateFormat.format(date);
   public ArrayList testTransactionLog = new ArrayList();
+  public ArrayList testPrinterStatement = new ArrayList();
 
   public ArrayList createTestTransactionLog() {
     testTransactionLog.add(strDate + " || 1000.00 || || 1000.00\n");
     testTransactionLog.add(strDate + " || 2000.00 || || 3000.00\n");
     testTransactionLog.add(strDate + " || || -500.00 || 2500.00\n");
     return testTransactionLog;
+  }
+
+  public ArrayList createTestPrinterStatement() {
+    testPrinterStatement.add("date || credit || debit || balance\n");
+    testPrinterStatement.add(strDate + " || || -500.00 || 2500.00\n");
+    testPrinterStatement.add(strDate + " || 2000.00 || || 3000.00\n");
+    testPrinterStatement.add(strDate + " || 1000.00 || || 1000.00\n");
+    return testPrinterStatement;
   }
 }
--- src/test/java/FeatureTest.java
+++ src/test/java/FeatureTest.java
@@ -1,5 +1,6 @@
 import main.java.TestHelper;
 import main.java.Account;
+import main.java.Printer;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -10,33 +11,45 @@ public class FeatureTest {
 
   private TestHelper testHelper;
   private Account account;
+  private Printer printer;
 
   @Before
   public void setup() {
     testHelper = new TestHelper();
     account = new Account();
+    printer = new Printer();
+    testHelper.createTestTransactionLog();
+    testHelper.createTestPrinterStatement();
   }
 
   @Test
   public void depostiIncreasesBalance() {
-      account.makeDeposit(1000.00);
-      assertTrue(account.currentBalance() == 1000.00);
+    account.makeDeposit(1000.00);
+    assertTrue(account.currentBalance() == 1000.00);
   }
 
   @Test
   public void withdrawalDecreasesBalance() {
-      account.makeDeposit(1000.00);
-      account.makeWithdrawal(1000.00);
-      assertTrue(account.currentBalance() == 0.00);
+    account.makeDeposit(1000.00);
+    account.makeWithdrawal(1000.00);
+    assertTrue(account.currentBalance() == 0.00);
   }
 
   @Test
   public void depositX2WithdrawalX1AddedToTransaction() {
-      testHelper.createTestTransactionLog();
-      account.makeDeposit(1000.00);
-      account.makeDeposit(2000.00);
-      account.makeWithdrawal(500.00);
-      assertTrue(account.currentBalance() == 2500.00);
-      assertEquals(testHelper.testTransactionLog, account.transaction.transactionLog);
+    account.makeDeposit(1000.00);
+    account.makeDeposit(2000.00);
+    account.makeWithdrawal(500.00);
+    assertTrue(account.currentBalance() == 2500.00);
+    assertEquals(testHelper.testTransactionLog, account.transaction.transactionLog);
+  }
+
+  @Test
+  public void printStatement() {
+    account.makeDeposit(1000.00);
+    account.makeDeposit(2000.00);
+    account.makeWithdrawal(500.00);
+    printer.printStatement(account.transaction.transactionLog);
+    assertEquals(testHelper.testPrinterStatement, printer.bankStatement);
   }
 }
